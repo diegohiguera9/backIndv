cb7a0102ac0b980e2829cb18b027bcfd2b87a0a7
Diego <drhiguerag@unal.edu.co>
2022-10-31 12:41:49 -0500

    new origins

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import passport from "passport";
-import session from 'express-session';
+import session from "express-session";
 import userRouter from "./api/user/user.route";
 import categoryRouter from "./api/category/category.router";
 import productRouter from "./api/product/product.router";
@@ -13,11 +13,20 @@ import errorHandler from "./middleware/errorHandler";
 dotenv.config();
 const app = express();
 
-import './middleware/authGoogle'
+import "./middleware/authGoogle";
 
 //middleware
 app.use(morgan("tiny"));
-app.use(cors({origin:'https://frontfogon-dxx7qsmma-diegohiguera9.vercel.app/', credentials:true}));
+app.use(
+  cors({
+    origin: [
+      "https://frontfogon-dxx7qsmma-diegohiguera9.vercel.app/",
+      "http://localhost:3000",
+      '186.28.25.144'
+    ],
+    credentials: true,
+  })
+);
 app.set("trust proxy", 1);
 app.use(express.json());
 app.use(
@@ -25,7 +34,8 @@ app.use(
     secret: "secretcode",
     resave: true,
     saveUninitialized: true,
-  }))
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -34,12 +44,14 @@ app.use(passport.session());
 app.use("/user", userRouter);
 app.use("/category", categoryRouter);
 app.use("/product", productRouter);
-app.get('/logout', function(req, res, next){
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      res.redirect('/');
-    });
+app.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
+});
 
 //middleware for errors
 app.use(errorHandler);
