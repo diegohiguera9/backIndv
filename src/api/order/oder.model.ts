2292b1e87eb31af739add22cb361bd8930909d0b
Diego <drhiguerag@unal.edu.co>
2022-11-05 17:15:12 -0500

    order model

--- src/api/order/oder.model.ts
+++ src/api/order/oder.model.ts
@@ -1,14 +1,23 @@
 import {Schema, Document, model} from 'mongoose'
 import { IProduct } from '../product/product.model'
+import { ITable } from '../table/table.model'
 
 export interface IOrder extends Document {
-    products: Array<IProduct['_id']>
+    products: Array<IProduct['_id']>;
+    table: ITable['_id'];
 }
 
 const orderSchema = new Schema({
     products: {
         type: [{type: Schema.Types.ObjectId, ref:'Product'}],
         required:true
+    }, 
+    table: {
+        type: Schema.Types.ObjectId,
+        ref: 'Table'
+    },
+    location: {
+        type: Object
     }
 },{timestamps:true})
 
